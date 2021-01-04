d1d464d6bd9be7266fd1c82a6fb7f82393d6a468
Zak Monnet <zakmonnet@gmail.com>
2021-01-04 13:42:32 -0600

    built initial routes and models

---
+++ client/src/utils/API.js
@@ -0,0 +1,23 @@
+//import axios from "axios";
+
+export default {
+  getBooks: function () {
+    console.log("Get Books");
+    return axios.get("/api/books");
+  },
+
+  getBook: function (id) {
+    console.log("Get Book");
+    return axios.get("/api/books/" + id);
+  },
+
+  deleteBook: function (id) {
+    console.log("Delete Book");
+    return axios.delete("/api/books/" + id);
+  },
+
+  saveBook: function (bookData) {
+    console.log("Save Book");
+    return axios.post("/api/books", bookData);
+  },
+};
--- server.js
+++ server.js
@@ -1,19 +1,25 @@
 const express = require("express");
-const path = require("path");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-if(process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build");)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
 }
 
-app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client/build/index.html"));
+app.use(routes);
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/booksearch", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is now listening on port ${PORT}!`);
-})
+  console.log(`Server is now listening on port ${PORT}!`);
+});
